c569f01c12b0e498b1f62043387073724e8ced8f
Rex <rexrock@gmail.com>
2012-10-08 15:46:22 +0200

    Start project 888 Internal Consulting

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InternalConsulting::Application.config.secret_token = '6214b9b72d77d35d22662fb11ce802a1e6cf283400d6887ea1d0c919668bbfe3065279ac3ace730b00961f99dabaf4f21dd793ce81681d88baef51e25b5e84d3'
